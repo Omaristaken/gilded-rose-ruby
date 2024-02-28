a5250b274baea6eed47cf03a064f1ed9aeda621f
Omar <bomarm567@gmail.com>
2024-02-28 01:49:41 -0600

    cleaned everything up

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,61 +1,18 @@
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
 
-  def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
-  end
+  class Item
+    attr_reader :quality, :days_remaining
 
-  if name == 'normal'
-    return normal_tick
-  end
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
 
-
-
-    if @quality != 0
-      if days_remaining > 0
-        @quality -= 1
-      end
-      if days_remaining <= 0
-        @quality -= 2
-      end
-      @days_remaining -= 1
+    def tick
     end
   end
 
-  def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0
-  end
-
-  def sulfuras_tick
-  end
-
-  def backstage_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    return @quality = 0 if @days_remaining = 0
-
-    @quality += 1
-    @quality += 1 if @days_remaining < 10
-    @quality += 1 if @days_remaining < 5
-  end
-
-
-
-
-
-
-  def normal_tick
-    @item = Normal.new(quality, days_remaining)
-    item.tick
-  end
 
   class Normal < Item
-
     def tick
       @days_remaining -= 1
       return if @quality == 0
@@ -65,24 +22,7 @@ class GildedRose
   end
 
 
-  def quality
-    return item.quality if item
-    @quality
-  end
-
-  def days_remaining
-    return item.days_remaining if item
-    @days_remaining
-  end
-
-
-  def brie_tick
-    @item = Brie.new(quality, days_remaining)
-    item.tick
-  end
-
   class Brie < Item
-
     def tick
       @days_remaining -= 1
       return if @quality >= 50
@@ -90,7 +30,6 @@ class GildedRose
       @quality += 1 if @days_remaining <= 0
     end
   end
-
 
 
   class Backstage < Item
@@ -104,66 +43,3 @@ class GildedRose
       @quality += 1 if @days_remaining < 5
     end
   end
-
-  class item
-    attr_reader :quality, :days_remaining
-
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-  end
-
-  def tick
-  end
-
-
-
-
-
-
-  def tick
-    if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
-      if @quality > 0
-        if @name != "Sulfuras, Hand of Ragnaros"
-          @quality = @quality - 1
-        end
-      end
-    else
-      if @quality < 50
-        @quality = @quality + 1
-        if @name == "Backstage passes to a TAFKAL80ETC concert"
-          if @days_remaining < 11
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality = @quality + 1
-            end
-          end
-        end
-      end
-    end
-    if @name != "Sulfuras, Hand of Ragnaros"
-      @days_remaining = @days_remaining - 1
-    end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != "Backstage passes to a TAFKAL80ETC concert"
-          if @quality > 0
-            if @name != "Sulfuras, Hand of Ragnaros"
-              @quality = @quality - 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality = @quality + 1
-        end
-      end
-    end
-  end
-end
