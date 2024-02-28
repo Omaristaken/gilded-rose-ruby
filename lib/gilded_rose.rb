2c330b35b1f4e2d95f5560cff274b579cca65250
Omar <bomarm567@gmail.com>
2024-02-28 01:11:52 -0600

    added backstage_tick function

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -33,6 +33,17 @@ class GildedRose
   def sulfuras_tick
   end
 
+  def backstage_tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    return @quality = 0 if @days_remaining = 0
+
+    @quality += 1
+    @quality += 1 if @days_remaining < 10
+    @quality += 1 if @days_remaining < 5
+  end
+
+
 
   end
   def tick
